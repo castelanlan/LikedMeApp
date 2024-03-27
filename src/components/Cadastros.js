9a1faab88944ae888e2379f211df978c39becd08
Gabriel Castelan <gabrielmcastelan@gmail.com>
2024-03-27 09:41:35 -0300

    cadastro

--- src/components/Cadastros.js
+++ src/components/Cadastros.js
@@ -4,16 +4,25 @@ import './cadastros.css'
 
 function Table() {
   // const usuarios = 
-  return(<></>)
+  return (<></>)
 }
 
 function Cadastros() {
   return (
     <div>
       <div className="card card-cadastros">
-        <h2>Usuários</h2>
-        <p>Mostrando 1 de 50 usuários</p>
-        <Table />
+        <div className="cdstro-header">
+          <div className="cdstro-usuario">
+            <h2>Usuários</h2>
+            <p>Mostrando 1 de 50 usuários</p>
+          </div>
+          <div className="cdstro">
+            <form>
+              <input type="textarea" id="pesquisa" placeholder="Pesquisar"/>
+              <svg className="cdstro-usuario-icon w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" ><path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>
+            </form>
+          </div>
+        </div>
       </div>
       <div className="card card-cadastros">
         <h2>Gabriel Castelan</h2>
